da6798c0558ef52c6700e0c4d937ec323a2a10d5
Tymek <tymek.gan37@gmail.com>
2026-09-15 10:24:32 +0200

    Update theme and add product data

---
+++ wordpress/wp-content/themes/Stronka/dane.php
@@ -0,0 +1,24 @@
+<?php
+$products = [
+    [
+        "nazwa" => "Koszulka",
+        "cena" => 149,
+        "zdjecie" => "pozniejbedziezdjecie.png",
+        "rozmiary" => ["S", "M", "L"]
+    ],
+    [
+        "nazwa" => "Spodnie",
+        "cena" => 299,
+         "zdjecie" => "pozniejbedziezdjecie.png",
+        "rozmiary" => ["S", "M", "L"]
+
+    ],
+      [
+        "nazwa" => "Bluza",
+        "cena" => 299,
+          "zdjecie" => "pozniejbedziezdjecie.png",
+        "rozmiary" => ["S", "M", "L", "XL"]
+
+    ]
+];
+?>
