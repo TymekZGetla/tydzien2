85868c8fd2f9fc6a4f7ac72e774d94c9312ed84b
Tymek <tymek.gan37@gmail.com>
2026-09-16 15:01:30 +0200

    I combined the CSS and lightly styled the menu

--- wordpress/wp-content/themes/Stronka/functions.php
+++ wordpress/wp-content/themes/Stronka/functions.php
@@ -3,21 +3,21 @@
 function pizzeria_pauza_styles() {
     wp_enqueue_style(
         'pizzeria-pauza-style',
-        get_stylesheet_uri()
+        get_template_directory_uri() . '/style.css',
+        array(),
+        '1.0'
     );
 }
 
 add_action('wp_enqueue_scripts', 'pizzeria_pauza_styles');
+if ( ! function_exists( 'mytheme_register_nav_menu' ) ) {
 
-if ( ! function_exists( 'mytheme_register_nav_menu' ) ) 
-    {
+    function mytheme_register_nav_menu() {
+        register_nav_menus( array(
+            'primary_menu' => __( 'Primary Menu', 'text_domain' ),
+            'footer_menu'  => __( 'Footer Menu', 'text_domain' ),
+        ) );
+    }
 
-	function mytheme_register_nav_menu()
-    {
-		register_nav_menus( array(
-	    	'primary_menu' => __( 'Primary Menu', 'text_domain' ),
-	    	'footer_menu'  => __( 'Footer Menu', 'text_domain' ),
-		) );
-	}
-	add_action( 'after_setup_theme', 'mytheme_register_nav_menu', 0 );
+    add_action( 'after_setup_theme', 'mytheme_register_nav_menu', 0 );
 }
--- wordpress/wp-content/themes/Stronka/header.php
+++ wordpress/wp-content/themes/Stronka/header.php
@@ -2,35 +2,34 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
     <?php wp_head(); ?>
+    
 </head>
-
 <body <?php body_class(); ?>>
 
 <header class="header">
+
     <h1>Marka odzieżowa</h1>
+
     <div class="menu">
+
         <button class="menu-button">
             <span class="material-symbols-outlined">menu</span>
         </button>
-<?php
-        wp_nav_menu
-        (  
-            array   
-            (
-'theme_location' => 'primary_menu', 
-'menu_class' => 'header-menu', 
-'container' => 'nav', 
-'container_class'=> 'menu-container',
-            ) 
-        );  
-?>
-            <a href="<?php echo home_url(); ?>">Strona główna</a>
-            <a href="/o-nas/">O nas</a>
-            <a href="/kontakt/">Kontakt</a>
-        </div>
+
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'primary_menu',
+                'menu_class' => 'header-menu',
+                'container' => 'nav',
+                'container_class' => 'menu-container',
+            )
+        );
+        ?>
+
     </div>
+
 </header>
