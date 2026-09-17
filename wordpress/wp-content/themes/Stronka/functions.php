5a1fd865bea5ad6c2b4779964b064017852cbbe4
Tymek <tymek.gan37@gmail.com>
2026-09-17 08:56:22 +0200

    I made an options page in ACF

--- wordpress/wp-content/themes/Stronka/functions.php
+++ wordpress/wp-content/themes/Stronka/functions.php
@@ -21,3 +21,14 @@ if ( ! function_exists( 'mytheme_register_nav_menu' ) ) {
 
     add_action( 'after_setup_theme', 'mytheme_register_nav_menu', 0 );
 }
+
+if( function_exists('acf_add_options_page') ) 
+    {
+    acf_add_options_page(array(
+        'page_title' => 'Ustawienia motywu',
+        'menu_title' => 'Ustawienia motywu',
+        'menu_slug' => 'theme-settings',
+        'capability' => 'edit_posts',
+        'redirect' => false
+    ));
+    }
