9e7dfd341fa375e810bf4d277013d39dd644acf7
Tymek <tymek.gan37@gmail.com>
2026-09-17 14:01:40 +0200

    I added the phone number to the page

--- wordpress/wp-content/themes/Stronka/header.php
+++ wordpress/wp-content/themes/Stronka/header.php
@@ -2,22 +2,32 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
     <?php wp_head(); ?>
-    
 </head>
+
 <body <?php body_class(); ?>>
 
 <header class="header">
 
     <?php
-$logo = get_field('logo', 'option');
+    $logo = get_field('logo', 'option');
 
-if ($logo) :
-?>
-    <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
-<?php endif; ?>
+    if ($logo) :
+    ?>
+        <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+    <?php endif; ?>
+
+    <?php
+    $telefon = get_field('telefon', 'option');
+
+    if ($telefon) :
+    ?>
+        <a href="tel:<?php echo esc_attr($telefon); ?>">
+            <?php echo esc_html($telefon); ?>
+        </a>
+    <?php endif; ?>
 
     <div class="menu">
 
